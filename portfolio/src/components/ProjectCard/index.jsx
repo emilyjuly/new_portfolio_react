5ea2f951210944acd5dd3086b38136b609ee74b3
Emily Domingos <emily.domingos@e-unicred.com.br>
2024-02-23 11:54:51 -0300

    Adicionando projeto subselect

--- portfolio/src/components/ProjectCard/index.jsx
+++ portfolio/src/components/ProjectCard/index.jsx
@@ -1,22 +1,22 @@
 import "./style.css";
 import {BsGithub} from "react-icons/bs";
 
-const ProjectCard = () => {
+const ProjectCard = ({ urlImage, link, title, description, resources, linkRepo}) => {
     return (
             <div className="card-project">
                 <div className="project-picture">
-                    <img src="/project-map-img.png" alt="Imagem preview do projeto"/>
+                    <img src={urlImage} alt="Imagem preview do projeto"/>
                 </div>
                 <p className="project-infos">
-                    <a href="https://ip-address-july.netlify.app/" target="_blank" title="Acesse o deploy do projeto">IP ADDRESS TRACKER</a>
-                    <span className="profissao">Um buscador de localização através do IP informado.</span>
-                    <span className="email">VUE 3 - NUXT 3 - PRIMEVUE - PINIA - API - VITE</span>
+                    <a href={link} target="_blank" title="Acesse o deploy do projeto">{title}</a>
+                    <span className="profissao">{description}</span>
+                    <span className="email">{resources}</span>
                 </p>
                 <div className="social-media">
-                    <a href="https://ip-address-july.netlify.app/" target="_blank" title="Acesse o deploy do projeto">
+                    <a href={link} target="_blank" title="Acesse o deploy do projeto">
                         <img src="/deploy.png" alt="Foguete" height="20px"/>
                     </a>
-                    <a href="https://github.com/emilyjuly/ip-address-tracker" target="_blank" title="Acesse o repositório do projeto">
+                    <a href={linkRepo} target="_blank" title="Acesse o repositório do projeto">
                         <BsGithub />
                     </a>
                 </div>
--- portfolio/src/pages/Projects/index.jsx
+++ portfolio/src/pages/Projects/index.jsx
@@ -4,7 +4,8 @@ import ProjectCard from "../../components/ProjectCard/index.jsx";
 const Projects = () => {
     return (
         <div className="container-projects">
-            <ProjectCard/>
+            <ProjectCard urlImage={'/project-subselect-img.png'} link={'https://sub-select.netlify.app/'} title={'Sub-select'} description={'Um buscador de cidades, que retorna a mesma no mapa, com o intuíto de exemplificar o uso de um sub-select'} resources={'VUE 3 - NUXT 3 - PRIMEVUE - PINIA - API - PRIMEFLEX'} linkRepo={'https://github.com/emilyjuly/sub-select'}/>
+            <ProjectCard urlImage={'/project-map-img.png'} link={'https://sub-select.netlify.app/'} title={'IP ADDRESS TRACKER'} description={'Um buscador de localização através do IP informado.'} resources={'VUE 3 - NUXT 3 - PRIMEVUE - PINIA - API - VITE'} linkRepo={'https://github.com/emilyjuly/ip-address-tracker'}/>
         </div>
     );
 }
