fc43120520511c5aba226778662f416d74f68ed9
Emily Domingos <emily.domingos@e-unicred.com.br>
2024-07-16 17:11:46 -0300

    Adiciona logo

--- portfolio/src/routes.jsx
+++ portfolio/src/routes.jsx
@@ -1,25 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from "./pages/Home";
-import Header from "./components/Header";
-import Projects from "./pages/Projects";
-import Certificates from "./pages/Certificates";
-import ContactMe from "./pages/ContactMe"
-import Help4You from "./pages/Help4you/index.jsx";
+import Home from './pages/Home';
+import Header from './components/Header';
+import Projects from './pages/Projects';
+import Certificates from './pages/Certificates';
+import ContactMe from './pages/ContactMe';
 
 function RoutesApp() {
-    return (
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/certificates" element={<Certificates />} />
-                <Route path="/help-4-you" element={<Help4You />} />
-                <Route path="/contact-me" element={<ContactMe />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/certificates" element={<Certificates />} />
+        <Route path="/contact-me" element={<ContactMe />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default RoutesApp;
